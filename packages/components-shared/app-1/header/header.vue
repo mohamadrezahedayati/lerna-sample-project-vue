2d18fbc0de1ac437e1b2b6a408c63abb88b4123b
mohamadrezahedayati <hedayatimohamadreza@gmail.com>
2021-05-05 10:24:24 +0430

    add package json to header

---
+++ packages/components-shared/app-1/header/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        header component
+        header component resuable
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
