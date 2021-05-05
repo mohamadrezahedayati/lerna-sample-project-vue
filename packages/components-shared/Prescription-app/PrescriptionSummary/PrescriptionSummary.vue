fe7399296a79a9aa29312faaf5fec6d4f076717b
mohamadrezahedayati <hedayatimohamadreza@gmail.com>
2021-05-05 17:40:19 +0430

    create prescription summary

---
+++ packages/components-shared/Prescription-app/PrescriptionSummary/PrescriptionSummary.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        prescription summary component
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
