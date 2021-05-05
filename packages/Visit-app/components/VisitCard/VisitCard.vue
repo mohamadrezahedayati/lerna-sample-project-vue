da277c4d49297941a12f205965ca708237f4e4a9
mohamadrezahedayati <hedayatimohamadreza@gmail.com>
2021-05-05 23:02:29 +0430

    create visit card component

---
+++ packages/Visit-app/components/VisitCard/VisitCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container-visit-card">
+        this is container visit card 
+        <br>
+        <PrescriptionSumList />
+    </div>
+</template>
+
+<script>
+    import PrescriptionSumList from '@mohamad-org/prescription-sum-list/PrescriptionSumList'
+    export default {
+        components:{
+            PrescriptionSumList
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+.container-visit-card{
+    border: 1px rgb(0, 81, 255) solid;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+</style>
--- packages/Visit-app/pages/index.vue
+++ packages/Visit-app/pages/index.vue
@@ -1,27 +1,8 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        visit-app
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div v-for="item in 5" :key="item">
+        <VisitCard />
       </div>
     </div>
   </div>
