617e3929eb1963544e348794bee55c54154de0c7
mohamadrezahedayati <hedayatimohamadreza@gmail.com>
2021-05-07 22:59:24 +0430

    add patient file to shell app

--- packages/shell-app/pages/index.vue
+++ packages/shell-app/pages/index.vue
@@ -1,34 +1,16 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        shell-app
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <PatientFile />
   </div>
 </template>
 
 <script>
-export default {}
+import PatientFile from '@mohamad-org/patient-file/PatientFile'
+export default {
+  components:{
+    PatientFile
+  }
+}
 </script>
 
 <style>
